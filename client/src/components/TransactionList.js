a28af6610dfd2eaf11adc611e179faa2793ad760
Yu Hao Wong <wongyuhao@gmail.com>
2020-11-30 01:38:26 +0800

    add additional Transaction schema and relevant form fields

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -4,16 +4,19 @@ import { Transaction } from './Transaction';
 import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
-  const { transactions, getTransactions } = useContext(GlobalContext);
-
+  const { transactions, getTransactions, loading } = useContext(GlobalContext);
+  let temp = <></>;
   useEffect(() => {
     getTransactions();
+
+    console.log(loading);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <>
       <h3>History</h3>
+      
       <ul className="list">
         {transactions.map(transaction => (<Transaction key={transaction._id} transaction={transaction} />))}
       </ul>
