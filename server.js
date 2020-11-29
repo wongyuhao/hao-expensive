b56780f0d47b2bf3017f126d068d966452503434
Yu Hao Wong <wongyuhao@gmail.com>
2020-11-29 23:47:30 +0800

    heroku deploy

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const transactions = require('./routes/transactions.js')
 dotenv.config({path: './config/config.env'});
 
 connectDB();
-const app = express();
-app.use(express.json());
 
-app.use('/api/v1/transactions', transactions)
+const app = express();
+
+app.use(express.json());
+app.use('/api/v1/transactions', transactions);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, console.log(`server: ${process.env.NODE_ENV} mode - port ${PORT}`));
