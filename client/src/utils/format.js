bc33b015035a5d4d76f0a8dd43c35c6342a72201
Yu Hao Wong <wongyuhao@gmail.com>
2020-12-17 15:09:26 +0800

    added arrayToOptions()

--- client/src/utils/format.js
+++ client/src/utils/format.js
@@ -1,3 +1,26 @@
+
+
 export function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+export function toTitleCase(str) {
+  return str.replace(
+    /\w\S*/g,
+    function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+  );
+}
+
+export function arrayToOptions(arr){
+  return (
+    arr.map(option => {
+      let obj = {};
+      obj['value'] = option.toUpperCase();
+      obj['label'] = option.toUpperCase();
+
+      return obj
+    })
+  )
 }
