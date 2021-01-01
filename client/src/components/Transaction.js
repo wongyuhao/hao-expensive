1853f5e53345aae59962d95c7b97615be21a907f
Yu Hao Wong <wongyuhao@gmail.com>
2020-12-31 16:51:21 -0800

    change delete and remark visibilty behavior

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -5,6 +5,7 @@ import { numberWithCommas, getSymbol, getCategoryColor, getSourceColor, dot } fr
 export const Transaction = ({ transaction }) => {
   const { deleteTransaction, enums } = useContext(GlobalContext);
   const [buttonClass, setButtonClass] = useState("hidden");
+  const [remarksClass, setRemarksClass] = useState("hidden")
   let income = 0;
   let expense = 0;
   if(transaction.amount > 0){
@@ -21,6 +22,14 @@ export const Transaction = ({ transaction }) => {
     }
   }
 
+  const toggleRemarksVisiblity =()=>{
+    if(remarksClass === 'hidden') {
+      setRemarksClass('')
+    }else{
+      setRemarksClass('hidden')
+    }
+  }
+
 
   return (
   <div className='flex  mx-2 my-1 flex-row flex-nowrap rounded-lg bg-red-600'>
@@ -31,12 +40,20 @@ export const Transaction = ({ transaction }) => {
     }} >
         ✕
     </button> 
-    <li className='p-2 flex flex-col flex-nowrap w-full bg-gray-800 border-gray-700 shadow-sm rounded ' onClick={()=>toggleButtonVisibility()}>
+    <li className='p-2 flex flex-col flex-nowrap w-full bg-gray-800 border-gray-700 shadow-sm rounded '>
       <div className= 'flex flex-row justify-between w-full' style={{borderRight:`3px solid ${getSourceColor(transaction.source, enums)}`}}>
 
         <div className='flex flex-row items-center'>
-        <div style={dot(getCategoryColor(transaction.category, enums))} />
-          <div className='font-semibold' >{transaction.text}</div> 
+          <div 
+            className='cursor-pointer'
+            style={dot(getCategoryColor(transaction.category, enums))}  
+            onClick={()=>toggleButtonVisibility()}
+          />
+          <div 
+            className={`font-semibold ${transaction.remarks ? 'cursor-pointer':""}`} 
+            onClick={()=>toggleRemarksVisiblity()} >
+              {transaction.text} {(transaction.remarks)? "..":""}
+          </div> 
         </div>
         
         <div className=' lg:w-2/5  flex flex-row justify-self-end pr-5'>
@@ -53,7 +70,13 @@ export const Transaction = ({ transaction }) => {
         </div>
       </div>
       
-      <div className='ml-6 text-sm text-gray-500'>{(transaction.remarks) ? "> " + transaction.remarks : ""}</div>
+      {
+        (transaction.remarks) ?
+        <div className={`ml-6 text-sm text-gray-500 ${remarksClass}`}>{ ">"  + transaction.remarks }</div>
+        :
+        <></>
+      }
+      
       
     </li>
 
